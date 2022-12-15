14d167be29b4823c7233f3c0c5da38bb38b357ac
Abhishek <abhishek.magdum.07@gmail.com>
2022-12-15 13:22:33 +0530

    first commit

---
+++ src/com/daytwoproblems/SpringSeason.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+//import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class SpringSeason {
+
+    // Write a program SpringSeason.java that takes two int values m and d from the command line and prints true if day d of month m is between March 20 (m = 3, d=20) and June 20 (m = 6, d = 20), false otherwise.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the month number =m :");
+        int m = sc.nextInt();
+        System.out.println("Enter the day = d :");
+        int d = sc.nextInt();
+
+
+        if ((m == 3 && d >= 20 && d <= 31) || (m == 4 && d >= 1 && d <= 30) || (m == 5 && d >= 1 && d <= 31) || (m == 6 && d <= 1 && d >= 20)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
+
